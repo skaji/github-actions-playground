fea7ea95ea5721828008e47a69cdec9e8a42c1c1
Shoichi Kaji <skaji@cpan.org>
2020-07-07 11:05:54 +0900

    wip

--- test.pl
+++ test.pl
@@ -1,83 +1,49 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use feature 'say';
+use File::Basename ();
+use File::Temp ();
+use File::Copy ();
 
-use Config;
-use HTTP::Daemon;
-use HTTP::Response;
-use HTTP::Tiny;
-use Socket ();
-use Sys::Hostname ();
+my @url = do {
+    open my $fh, "<", "url.txt" or die;
+    my @line = <$fh>;
+    chomp @line;
+    @line;
+};
 
-{   # hotfix
-    no warnings 'redefine';
-    *HTTP::Daemon::url = sub {
-        my $self = shift;
+warn "-> which curl\n";
+system "which", "-a", "curl";
+warn "-> curl --version\n";
+system "curl", "--version";
 
-        my $host = $self->sockhost;
-        $host = '127.0.0.1' if $host eq '0.0.0.0';
-        $host = '::1'       if $host eq '::';
-        $host = "[$host]"   if $self->sockdomain == Socket::AF_INET6;
 
-        my $url = sprintf "%s://%s", $self->_default_scheme, $host;
-        my $port = $self->sockport;
-        $url .= ":$port" if $port != $self->_default_port;
-        $url .= "/";
-        $url;
-    };
-}
-
-sub test {
-    my $host = shift;
-    say "Create HTTP::Daemon object with LocalHost => ", $host // "undef";
-    my $httpd = HTTP::Daemon->new(LocalHost => $host);
-    if (!$httpd) {
-        say "Fail, $!";
-        return;
+for my $url (@url) {
+    my $basename = File::Basename::basename $url;
+    my $file = "header.txt";
+    unlink $file;
+    my @cmd = qw(
+        curl
+        --location
+        --silent
+        --show-error
+        --max-time 60
+        --max-redirs 5
+        --user-agent HTTP-Tinyish/test
+    );
+    push @cmd,
+        "-z", $basename,
+        "-o", $basename,
+        "--dump-header", $file,
+        "--remote-time",
+        $url
+    ;
+    warn "-> $url\n";
+    my $exit = system @cmd;
+    if ($exit != 0) {
+        open my $fh, "<", $file or die;
+        File::Copy::copy($fh, \*STDOUT);
+        die;
     }
-    my $url = $httpd->url;
-    say "\$httpd->url is $url";
-
-    my $pid = fork // die;
-    if ($pid == 0) {
-        say "Perform HTTP GET $url";
-        my $res;
-        eval {
-            local $SIG{ALRM} = sub { die "alarm\n" };
-            alarm 5;
-            $res = HTTP::Tiny->new(timeout => 5)->get($url);
-        };
-        my $err = $@ ? "timeout" : undef;
-        alarm 0;
-        if ($err) {
-            say "Error: $err";
-        } else {
-            say "$res->{status} $res->{reason}";
-            say $res->{content} if $res->{status} == 599;
-        }
-        exit;
-    }
-
-    if (my $c = $httpd->accept) {
-        my $req = $c->get_request;
-        $c->send_response(HTTP::Response->new(200));
-        $c->close;
-    }
-    $httpd->close;
-
-    wait;
 }
 
-$|++;
-
-say "perl $^V $Config{archname}";
-for my $class (qw(Socket IO::Socket::IP HTTP::Daemon)) {
-    say "$class ", $class->VERSION;
-}
-say "";
-
-for my $host (undef, qw(localhost 127.0.0.1 ::1), Sys::Hostname::hostname) {
-    test $host;
-    say "";
-}
