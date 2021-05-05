72778a531bec4daec3da30d34993c491df076e12
Shoichi Kaji <skaji@cpan.org>
2021-05-05 15:57:23 +0900

    wip

--- test.pl
+++ test.pl
@@ -1,49 +1,35 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use File::Basename ();
-use File::Temp ();
-use File::Copy ();
+use HTTP::Tiny;
+use IO::Socket::SSL;
 
-my @url = do {
-    open my $fh, "<", "url.txt" or die;
-    my @line = <$fh>;
-    chomp @line;
-    @line;
-};
-
-warn "-> which curl\n";
-system "which", "-a", "curl";
-warn "-> curl --version\n";
-system "curl", "--version";
+system 'perl -MWin32 -E "say Win32->VERSION"';
 
 
-for my $url (@url) {
-    my $basename = File::Basename::basename $url;
-    my $file = "header.txt";
-    unlink $file;
-    my @cmd = qw(
-        curl
-        --location
-        --silent
-        --show-error
-        --max-time 60
-        --max-redirs 5
-        --user-agent HTTP-Tinyish/test
-    );
-    push @cmd,
-        "-z", $basename,
-        "-o", $basename,
-        "--dump-header", $file,
-        "--remote-time",
-        $url
-    ;
-    warn "-> $url\n";
-    my $exit = system @cmd;
-    if ($exit != 0) {
-        open my $fh, "<", $file or die;
-        File::Copy::copy($fh, \*STDOUT);
-        die;
-    }
+
+my $res = HTTP::Tiny->new->mirror(
+    "https://cpan.metacpan.org/authors/id/J/JD/JDB/Win32-0.57.tar.gz",
+    "Win32-0.57.tar.gz",
+);
+
+die $res->{status} if !$res->{success};
+
+!system "tar xf Win32-0.57.tar.gz" or die;
+
+chdir "Win32-0.57" or die;
+
+system "perl Makefile.PL";
+warn "-> \$? = $?\n";
+
+if (-f "Makefile") {
+    warn "Found Makefile";
+} else {
+    warn "NOT found Makefile";
 }
+
+system "gmake";
+warn "-> \$? = $?\n";
+
+
 
