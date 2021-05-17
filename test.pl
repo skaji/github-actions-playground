b34717c92bed953c02e8156d84b2bb2cfc24de8c
Shoichi Kaji <skaji@cpan.org>
2021-05-18 02:12:25 +0900

    hoge

--- test.pl
+++ test.pl
@@ -3,13 +3,6 @@ use strict;
 use warnings;
 use IPC::Run3 ();
 
-IPC::Run3::run3 [$^X, "-MO=Deparse", "-e", <<'EOF'], undef, undef, undef;
-print "Hello, World1!\n";
-EOF
-print "exit code $?\n";
-
-system $^X, "-MO=Deparse", "-e", <<'EOF';
-print "Hello, World2!\n";
-EOF
+system $^X, "-MO=Deparse", "-e", "print 1";
 
 print "exit code $?\n";
