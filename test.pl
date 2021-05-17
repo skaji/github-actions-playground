ad6cd0b55510e81103215b3e7e7368f0fe3eef68
Shoichi Kaji <skaji@cpan.org>
2021-05-18 01:51:00 +0900

    hoge

--- test.pl
+++ test.pl
@@ -1,9 +1,15 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
+use IPC::Run3 ();
+
+IPC::Run3::run3 [$^X, "-e", <<'EOF'], undef, undef, undef;
+print "Hello, World1!\n";
+EOF
+print "exit code $?\n";
 
 system $^X, "-e", <<'EOF';
-print "Hello, World!\n";
+print "Hello, World2!\n";
 EOF
 
 print "exit code $?\n";
