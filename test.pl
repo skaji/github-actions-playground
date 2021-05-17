7f6df727541223cb0a11799538ff51157edf21ab
Shoichi Kaji <skaji@cpan.org>
2021-05-18 00:33:58 +0900

    hoge

--- test.pl
+++ test.pl
@@ -1,35 +1,7 @@
 #!/usr/bin/env perl
 use strict;
 use warnings;
-use HTTP::Tiny;
-use IO::Socket::SSL;
 
-system 'perl -MWin32 -E "say Win32->VERSION"';
-
-
-
-my $res = HTTP::Tiny->new->mirror(
-    "https://cpan.metacpan.org/authors/id/J/JD/JDB/Win32-0.57.tar.gz",
-    "Win32-0.57.tar.gz",
-);
-
-die $res->{status} if !$res->{success};
-
-!system "tar xf Win32-0.57.tar.gz" or die;
-
-chdir "Win32-0.57" or die;
-
-system "perl Makefile.PL";
-warn "-> \$? = $?\n";
-
-if (-f "Makefile") {
-    warn "Found Makefile";
-} else {
-    warn "NOT found Makefile";
-}
-
-system "gmake";
-warn "-> \$? = $?\n";
-
-
-
+system $^X, "-e", <<'EOF';
+print "hello world\n";
+EOF
