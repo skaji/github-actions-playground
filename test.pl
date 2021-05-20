25116dd6bb3151ceed1a4c3b9b03a641820fd1ae
Shoichi Kaji <skaji@cpan.org>
2021-05-20 23:57:17 +0900

    hoge

--- test.pl
+++ test.pl
@@ -1,8 +1,23 @@
-#!/usr/bin/env perl
 use strict;
 use warnings;
-use IPC::Run3 ();
 
-system $^X, "-MO=Deparse", "-e", "print 1";
+my @a;
+push @a, q[print "0\x0d\x0a"];
+push @a, q[print "1\x0a"];
+push @a, q[print "2\x0d"];
 
-print "exit code $?\n";
+my $x = <<'EOF';
+print "3\n";
+EOF
+push @a, $x;
+
+my $y = <<'EOF';
+print "4\n";
+EOF
+push @a, $y;
+
+for my $i (0 .. $#a) {
+    print "[$i] START\n";
+    system $^X, "-e", $a[$i];
+    print "[$i] END\n";
+}
