cb62e08ee5d2d1f80fea4ce081990be6619335ee
Shoichi Kaji <skaji@cpan.org>
2021-11-24 21:10:49 +0900

    hoge

--- test.pl
+++ test.pl
@@ -1,36 +1,11 @@
 use strict;
 use warnings;
+use Capture::Tiny qw(capture);
 
-my @a;
-push @a, q[print "0\x0d\x0a"];
-push @a, q[print "1\x0a"];
-push @a, q[print "2\x0d"];
+my ($stdout, $stderr, $exit) = capture {
+    system $^X, "-e", 'for (1..5) { print STDOUT "out $_\n"; print STDERR "err $_\n" } exit 2';
+};
 
-my $x = <<'EOF';
-print "3\n";
-EOF
-push @a, $x;
-
-my $y = <<'EOF';
-print "4\n";
-EOF
-push @a, $y;
-
-push @a, "say q(5)";
-push @a, "say q('6)";
-push @a, q[say q("7)];
-push @a, q[say q(\8)];
-
-push @a, <<'EOF';
-say 9;
-EOF
-
-push @a, <<'EOF';
-say qq[10\n];
-EOF
-
-for my $i (0 .. $#a) {
-    print "[$i] START\n";
-    system $^X, "-E", $a[$i];
-    print "[$i] END\n";
-}
+print "STDOUT >>>>$stdout<<<<\n";
+print "STDERR >>>>$stderr<<<<\n";
+print "EXIT   >>>>$exit<<<<\n";
