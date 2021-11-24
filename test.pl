3b0ec2507b7655d8bd69ec85e9df5ed94707789b
Shoichi Kaji <skaji@cpan.org>
2021-11-24 21:16:41 +0900

    hoge

--- test.pl
+++ test.pl
@@ -2,7 +2,21 @@ use strict;
 use warnings;
 use Capture::Tiny qw(capture);
 
+
+{
+    print "cpanm\n";
+    my $exit = system "cpanm";
+    print "EXIT >>>>$exit, $?<<<<\n";
+}
+{
+    print "cpanm --unknown\n";
+    my $exit = system "cpanm", "--unknown";
+    print "EXIT >>>>$exit, $?<<<<\n";
+}
+
+
 for my $cmd (["cpanm"], ["cpanm", "--unknown"]) {
+    print "$cmd\n";
     my ($stdout, $stderr, $exit) = capture {
         system @$cmd;
     };
